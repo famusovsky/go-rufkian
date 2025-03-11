3cd36d1bb42e986468bb21bc907d403ad4cd488c
Alexey Stepanov <st3p-by-step@ya.ru>
2025-03-12 02:55:24 +0300

    updated woerter proxy

--- internal/companion/proxy/const.go
+++ internal/companion/proxy/const.go
@@ -1,6 +1,36 @@
 package proxy
 
 const (
-	woerterURL           = "https://www.woerter.net/?w="
-	woerterWordElementID = "rAbschnitt "
+	woerterURL     = "https://www.woerter.net/?w="
+	styleElementID = "text/css"
+)
+
+var (
+	classesToReturn = []string{
+		"rAbschnitt ", "rInfo",
+	}
+
+	elementsWithTypeToExclude = []string{
+		"nav", "svg",
+	}
+
+	elementsWithClassToExclude = []string{
+		"rKnpf", "rRechts",
+	}
+
+	elementsWithTypeToPop = []string{
+		"section",
+	}
+
+	attributesToSave = []string{
+		"style",
+	}
+
+	languagesToSave = []string{
+		"ru", "en",
+
+		// just for fun
+		"uk", "be",
+		"he", "ar",
+	}
 )
--- internal/companion/proxy/handlers.go
+++ internal/companion/proxy/handlers.go
@@ -1,6 +1,10 @@
 package proxy
 
 import (
+	"slices"
+	"strings"
+	"sync"
+
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
 	"golang.org/x/net/html"
@@ -40,35 +44,142 @@ func (h *handlers) Woerter(c *fiber.Ctx) error {
 			return c.SendStatus(fiber.StatusNotFound)
 		}
 
-		var found bool
-		var f func(*html.Node)
-		f = func(n *html.Node) {
-			attrs := n.Attr
-			for _, attr := range attrs {
-				if attr.Key == "class" && attr.Val == woerterWordElementID {
-					// TODO remove images from the html, add corresponding css
-					err = html.Render(c, n)
-					found = true
-					return
-				}
-			}
-			for c := n.FirstChild; c != nil; c = c.NextSibling {
-				f(c)
-			}
+		definition := findArticle(page)
+
+		if definition == nil {
+			return c.SendStatus(fiber.StatusNotFound)
 		}
-		f(page)
+
+		removeExcessiveHighChildren(definition)
+		cleanInsides(definition)
+
+		err = html.Render(c, definition)
 
 		if err != nil {
-			h.logger.Error("render woerter word html", zap.Error(err))
-			return c.SendStatus(fiber.StatusNotFound)
-		}
-
-		if !found {
-			return c.SendStatus(fiber.StatusNotFound)
+			h.logger.Error("write word definition and style from buffer to ctx", zap.Error(err))
+			return fiber.ErrInternalServerError
 		}
 
 		return c.SendStatus(fiber.StatusOK)
 	}
 
 	return c.SendStatus(r.StatusCode())
+}
+
+func findArticle(n *html.Node) *html.Node {
+	var res *html.Node
+	var f func(*html.Node)
+	wg := &sync.WaitGroup{}
+	f = func(n *html.Node) {
+		defer wg.Done()
+		if n.Data == "article" {
+			res = n.FirstChild
+			return
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			wg.Add(1)
+			go f(c)
+		}
+	}
+	wg.Add(1)
+	go f(n)
+	wg.Wait()
+
+	return res
+}
+
+func removeExcessiveHighChildren(n *html.Node) {
+	excessiveHighNodes := []*html.Node{}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		var ok bool
+		for _, attr := range c.Attr {
+			if attr.Key == "class" && slices.Contains(classesToReturn, attr.Val) {
+				ok = true
+				break
+			}
+		}
+		if !ok {
+			excessiveHighNodes = append(excessiveHighNodes, c)
+		}
+	}
+	for _, excessiveNode := range excessiveHighNodes {
+		n.RemoveChild(excessiveNode)
+	}
+}
+
+func cleanInsides(n *html.Node) {
+	wg := sync.WaitGroup{}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		defer wg.Done()
+
+		if n.FirstChild == nil {
+			return
+		}
+
+		toExclude := []*html.Node{}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			if checkIfMustExcludeNode(c) {
+				toExclude = append(toExclude, c)
+			} else {
+				wg.Add(1)
+				go f(c)
+			}
+
+			c.Attr = slices.DeleteFunc(c.Attr, func(attr html.Attribute) bool {
+				return !slices.Contains(attributesToSave, attr.Key)
+			})
+		}
+
+		for _, c := range toExclude {
+			n.RemoveChild(c)
+		}
+		if checkIfMustPopNode(n) {
+			popNode(n)
+		}
+	}
+	wg.Add(1)
+	go f(n)
+	wg.Wait()
+}
+
+func checkIfMustExcludeNode(n *html.Node) bool {
+	if n.FirstChild == nil && n.Type == html.TextNode && (n.Data == "\n" || n.Data == "") {
+		return true
+	}
+	if slices.Contains(elementsWithTypeToExclude, n.Data) {
+		return true
+	}
+	for _, attr := range n.Attr {
+		if attr.Key == "class" {
+			for _, v := range strings.Split(attr.Val, " ") {
+				if slices.Contains(elementsWithClassToExclude, v) {
+					return true
+				}
+			}
+		}
+		if attr.Key == "lang" && !slices.Contains(languagesToSave, attr.Val) {
+			return true
+		}
+	}
+	return false
+}
+
+func checkIfMustPopNode(n *html.Node) bool {
+	return slices.Contains(elementsWithTypeToPop, n.Data)
+}
+
+func popNode(n *html.Node) {
+	if n.Parent == nil {
+		return
+	}
+	children := []*html.Node{}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		children = append(children, c)
+	}
+	for _, c := range children {
+		n.RemoveChild(c)
+		n.Parent.InsertBefore(c, n)
+	}
+	n.Parent.RemoveChild(n)
 }
